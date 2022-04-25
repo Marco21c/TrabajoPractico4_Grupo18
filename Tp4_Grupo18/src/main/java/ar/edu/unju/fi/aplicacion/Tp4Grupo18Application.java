0769ad7c1311fa2ae1e7f494b09b37f0c98e402f
MarcosC_18 <MarcosC_18@LAPTOP-PF4EE2SM>
2022-04-25 20:05:05 -0300

    Trabajo Practico 4

---
+++ Tp4_Grupo18/src/main/java/ar/edu/unju/fi/aplicacion/Tp4Grupo18Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Grupo18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4Grupo18Application.class, args);
+	}
+
+}
---
+++ Tp4_Grupo18/src/test/java/ar/edu/unju/fi/aplicacion/Tp4Grupo18ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4Grupo18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
